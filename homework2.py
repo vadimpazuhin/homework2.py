298cbd115c6f394b7fc3c77df8def6e00aba6829
vadimpazuhin <vadim2002@yandex.ru>
2024-07-07 03:25:29 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_of_completed_homework = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = (number_of_hours_spent / number_of_completed_homework)
+print('Курс:',course_name,',','всего задач:',number_of_completed_homework,',','затрачено часов:',number_of_hours_spent,',','среднее время выполнения:',time_for_one_task,'часа.')
